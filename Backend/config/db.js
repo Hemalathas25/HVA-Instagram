f00ef627c82632a9bc307ba427a77fe6e0e2c6e4
Hemalathas25 <hemalathasm04@gmail.com>
2024-03-22 08:57:08 +0530

    rectifying error

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         const con = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${con.connection.hst}`);
+        console.log(`MongoDB Connected: ${con.connection.host}`);
     } catch (error) {
         console.log(`Error: ${error.message}`);
         process.exit(1);
