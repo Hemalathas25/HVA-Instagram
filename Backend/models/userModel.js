063101a4fc8cfaa2499feb0feba82fb1dba52a91
Hemalathas25 <hemalathasm04@gmail.com>
2024-03-22 11:01:02 +0530

    adding UserModel

---
+++ Backend/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    email: { type: String, required: true, unique: true},
+
+    password: { type: String, required: true},
+
+    emailOrUsername: { type: String, required: true }, 
+
+    name: { type: String, required: true },
+
+    dateOfBirth: { type: Date, required: true },
+
+    userName: { type: String, required: true, unique: true},
+   
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the User model
+   
+    photo: { type: String }, // You can store the URL of the photo
+
+    bio: { type: String },
+    
+    website: { type: String }
+})
+
+const User = mongoose.model('User', UserSchema);
+
+export { User }
